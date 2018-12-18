1ab923d0e12e800aeb0b6ac82e7d66cca447c455
lourdes.zamora@unitec.edu <lourdes.zamora@unitec.edu>
2018-12-18 15:52:43 -0600

    whatever

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,7 @@ import {
   Button,
   StyleSheet,
   View,
+  Text
 } from 'react-native';
 import t from 'tcomb-form-native';
 const Contact = t.struct({
@@ -12,22 +13,38 @@ const Contact = t.struct({
 
 const Form = t.form.Form;
 export default class HomeScreen extends React.Component {
+  constructor(props){
+    super(props);
+   // this.handleSubmit= this.handleSubmit.bind(this);
+    this.state = {
+      contacts: []
+    };
+  }
+
   static navigationOptions = {
     header: null,
   };
   handleSubmit = () => {
     const value = this._form.getValue();
-    console.log('value',value);
+    console.log(value);
+    this.setState({ contacts: [...this.state.contacts, value]});
   }
   render() {
-    return (
+    const { contacts } =this.state;
+    return (  
       <View style={styles.container}>
         <Form type={Contact} ref={c => this._form =c} />
         <Button 
         title='Add' 
         onPress={this.handleSubmit}
-        />
+        /> 
+        <View style={styles.container}>
+        { contacts.map((item, key)=>(
+         <Text > { item.name } </Text>)
+         )}
+        </View>
       </View>
+     
     );
   }
 
